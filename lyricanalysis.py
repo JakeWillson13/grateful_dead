460945e14dbeef542e01c814be25c247e6937150
JakeWillson13 <willsojj@sunypoly.edu>
2025-07-21 21:33:13 -0400

    Update lyricanalysis.py

--- lyricanalysis.py
+++ lyricanalysis.py
@@ -126,29 +126,47 @@ def main():
     )
     st.plotly_chart(fig_top, use_container_width=True)
 
-    # Word Cloud for Top 50 Lyrics
-    st.subheader("Word Cloud: Top 50 Songs")
-    all_lyrics_top50 = ' '.join(merged['lyrics'].tolist())
-    words_top50 = re.findall(r"\b\w+\b", all_lyrics_top50.lower())
-    stopwords_set = set(STOPWORDS)
-    custom_stopwords = set([
-        'like','know','don','t','got','get','gotta','come','going','gonna',
-        'said','just','one','see','well','little','say','man','can','back',
-        'tell','never','always','around','dead','grateful'
-    ])
-    stopwords_set.update(custom_stopwords)
-    filtered_words = [w for w in words_top50 if w not in stopwords_set]
-    word_counts = Counter(filtered_words)
-    wc = WordCloud(
-        width=800, height=400,
-        background_color='white',
-        stopwords=stopwords_set,
-        min_font_size=10
-    ).generate_from_frequencies(word_counts)
-    fig_wc, ax = plt.subplots(figsize=(8, 4))
-    ax.imshow(wc, interpolation='bilinear')
-    ax.axis('off')
-    st.pyplot(fig_wc)
+# Word Cloud for Top 50 Lyrics
+st.subheader("Word Cloud: Top 50 Songs")
+all_lyrics_top50 = ' '.join(merged['lyrics'].tolist())
+words_top50 = re.findall(r'\b\w+\b', all_lyrics_top50.lower())
+
+stopwords_set = set(STOPWORDS)
+custom_stopwords = {
+    'like', 'know', 'don', 't', 'got', 'get', 'gotta', 'come', 'going', 'gonna',
+    'said', 'just', 'one', 'see', 'well', 'little', 'say', 'man', 'can', 'back',
+    'tell', 'never', 'always', 'around', 'dead', 'grateful'
+}
+stopwords_set.update(custom_stopwords)
+
+filtered_words = [w for w in words_top50 if w not in stopwords_set]
+word_counts = Counter(filtered_words)
+
+import matplotlib.cm as cm
+import matplotlib.colors as mcolors
+
+plasma = cm.get_cmap('plasma')
+norm = mcolors.Normalize(vmin=0, vmax=max(word_counts.values()))
+
+def color_func(word, font_size, position, orientation, random_state=None, **kwargs):
+    rgba = plasma(norm(word_counts[word]))
+    r, g, b = [int(x * 255) for x in rgba[:3]]
+    return f'#{r:02x}{g:02x}{b:02x}'
+
+wc = WordCloud(
+    width=800, height=400,
+    background_color=None,
+    mode='RGBA',
+    stopwords=stopwords_set,
+    min_font_size=10,
+    color_func=color_func
+).generate_from_frequencies(word_counts)
+
+fig_wc, ax = plt.subplots(figsize=(10, 5))
+ax.imshow(wc, interpolation='bilinear')
+ax.axis('off')
+st.pyplot(fig_wc)
+
 
 if __name__ == '__main__':
     main()
